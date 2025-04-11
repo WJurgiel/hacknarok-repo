273edb3d5547cc3154442f183bb876a967fffcb0
WJurgiel <wjurgiel@student.agh.edu.pl>
2025-04-11 20:18:08 +0200

    Simple REST controller created + containerized + connecting server and client

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,7 +1,30 @@
 import {Button} from "@mantine/core";
-
+import {useEffect, useRef, useState} from "react";
+import axios from 'axios'
 export const HomePage = () => {
-    return(
-        <Button variant="filled">Button</Button>
-    )
+    const [timeStamp, setTimeStamp] = useState("");
+    const [buttonClick, setButtonClick] = useState<boolean>(false);
+    const hasMounted = useRef(false);
+
+    useEffect(() => {
+        if (hasMounted.current) {
+            axios.get(
+                "http://localhost:8080/api/v1/home"
+            ).then(response => setTimeStamp(response.data))
+        } else {
+            hasMounted.current = true;
+        }
+    }, [buttonClick]);
+
+    const click = () => {
+        setButtonClick(prev => !prev);
+    };
+
+    return (
+        <>
+            <Button variant="filled" onClick={click}>Button</Button>
+            <br/>
+            <span>{timeStamp}</span>
+        </>
+    );
 }
