fe36bc5b00bb1f78b001bf8f3c94c56c24eed950
Wojciech Jurgielewicz <147532481+WJurgiel@users.noreply.github.com>
2025-04-11 20:29:04 +0200

    Server prep
    * simple rest controller (to test connection)
    * communication server-client
    * containerized server and frontend (communicates nicely)

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,4 +1,32 @@
 import {Button} from "@mantine/core";
+import {useEffect, useRef, useState} from "react";
+import axios from 'axios'
+export const HomePage = () => {
+    const [timeStamp, setTimeStamp] = useState("");
+    const [buttonClick, setButtonClick] = useState<boolean>(false);
+    const hasMounted = useRef(false);
+
+    useEffect(() => {
+        if (hasMounted.current) {
+            axios.get(
+                "http://localhost:8080/api/v1/home"
+            ).then(response => setTimeStamp(response.data))
+        } else {
+            hasMounted.current = true;
+        }
+    }, [buttonClick]);
+
+    const click = () => {
+        setButtonClick(prev => !prev);
+    };
+
+    return (
+        <>
+            <Button variant="filled" onClick={click}>Button</Button>
+            <br/>
+            <span>{timeStamp}</span>
+        </>
+    );
 
 export const HomePage = () => {
     return(
