9ff638e8df8135c0a5612d7536ddcc201310ba1b
noel crimson <michal.kolezynski@gmail.com>
2025-04-12 15:53:37 +0200

    registration template

--- server/src/main/java/org/hattivati/server/controllers/MainController.java
+++ server/src/main/java/org/hattivati/server/controllers/MainController.java
@@ -1,17 +1,20 @@
 package org.hattivati.server.controllers;
 
 import org.hattivati.server.ServerApplication;
+import org.hattivati.server.dto.registrationFormDTO;
+import org.hattivati.server.service.MainService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
 @RequestMapping("api/v1/")
 public class MainController {
+
+    @Autowired
+    private MainService mainService;
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
@@ -22,4 +25,8 @@ public class MainController {
         return "Hello from server at " + new java.util.Date();
     }
 
+    @PostMapping("/register/form")
+    public User registerUser(@RequestBody registrationFormDTO userDTO) {
+        return mainService.createUser(userDTO);
+    }
 }
