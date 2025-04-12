0fa6a3813f3c11638fe83898b12b06100a598bec
noel crimson <michal.kolezynski@gmail.com>
2025-04-13 00:00:41 +0200

    added gemini translation and feedback endpoints

--- server/src/main/java/org/hattivati/server/controllers/GeminiController.java
+++ server/src/main/java/org/hattivati/server/controllers/GeminiController.java
@@ -1,5 +1,9 @@
 package org.hattivati.server.controllers;
 
+import org.hattivati.server.dto.geminiDTO;
+import org.hattivati.server.dto.sendmessageDTO;
+import org.hattivati.server.service.GeminiService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -17,34 +21,22 @@ import java.util.Map;
 @RequestMapping("/ai")
 public class GeminiController {
 
-    private final RestTemplate restTemplate = new RestTemplate();
-    private final String apiKey = "AIzaSyB_jB24xsOrOFxPbd09q60sKb2rt4mR3aA";
+    @Autowired
+    private GeminiService geminiService;
 
     @PostMapping("/ask")
-    public String askGemini(@RequestBody String userPrompt) {
-        String url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + apiKey;
-
-        Map<String, Object> message = Map.of(
-                "contents", List.of(
-                        Map.of("parts", List.of(
-                                Map.of("text", userPrompt)
-                        ))
-                )
-        );
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(message, headers);
-
-        ResponseEntity<Map> response = restTemplate.postForEntity(url, entity, Map.class);
-
-        Map responseBody = response.getBody();
-        List<Map> candidates = (List<Map>) responseBody.get("candidates");
-        Map firstCandidate = candidates.get(0);
-        Map content = (Map) firstCandidate.get("content");
-        List<Map> parts = (List<Map>) content.get("parts");
-
-        return (String) parts.get(0).get("text");
+    public String sendMessage(@RequestBody String text) {
+        return geminiService.askGemini(text);
     }
+
+    @PostMapping("/translate")
+    public String translate(@RequestBody geminiDTO dto) {
+        return geminiService.askGemini(dto.getText() + " - translate into " + dto.getUserLanguage() + ", and don't say anything else.");
+    }
+
+    @PostMapping("/feedback")
+    public String feedback(@RequestBody geminiDTO dto) {
+        return geminiService.askGemini("'" + dto.getText() + "' - analyze the previous message, and give me feedback only in " + dto.getUserLanguage() + "  on grammar, spelling and punctuation in original language. Limit response to 3 sentences. Use original message to explain what was wrong.");
+    }
+
 }
